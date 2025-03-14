7d051c5a3cd089738cbd99126aeebb6c122fdbe7
Vincent Payet <vincentp9@outlook.fr>
2025-03-14 15:21:11 +0400

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kasa/', // Set the base URL for GitHub Pages
   build: {
     chunkSizeWarningLimit: 1000, // augmente la limite à 1000 Ko ou plus si nécessaire
   },
-})
+});
