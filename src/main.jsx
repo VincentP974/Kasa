6c41f054e2f9d0e2fa7910f817c3a1af293cd27d
Vincent Payet <vincentp9@outlook.fr>
2025-03-14 15:14:08 +0400

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/Router'; // Importe le router configuré
+import router from './routes/Router'; // Importe le router configuré
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
